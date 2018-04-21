ddd5563c632726c30d97e223e0ee8eb6460219fb
Josue Contreras <josudaco@hotmail.com>
2018-04-21 07:42:29 -0400

    Using bot modules (tx and Rx) on each node. Works! Now have to do TDMA and the web sharing.

--- Agregator Node/src/main.cpp
+++ Agregator Node/src/main.cpp
@@ -1,25 +1,28 @@
-#include <Arduino.h>
-//#include <UltraSonicSensor.h>
-//#include <Pins.h>
-
 /*
-Agreggator code for Final Project
-ECE2305
-By Josue Contreras
+  Agreggator code for Final Project
+  Team 14
+  ECE2305
+  By Josue Contreras
+  Contibutors Fivos, Anastasia, John
 */
 
+#include <Arduino.h>
 // Include RadioHead Amplitude Shift Keying Library
 #include <RH_ASK.h>
 // Include dependant SPI Library
 #include <SPI.h>
 //All pins easy accesses
 #include "globalPins.h"
+//Other classes
+
 
 // Create Amplitude Shift Keying Object
 RH_ASK rf_driver;
 
 // Fucntion Prototypes
 bool processData(String dataR);
+void printLoadingMessages(String message);
+void testAgreggator();
 
 //String Variables
 String recievedData;
@@ -32,9 +35,11 @@ int ind1;
 int ind2;
 int ind3;
 int ind4;
+const char *msg;
 
 //Other varibales
-int count;
+int count = 0;
+enum State {TRANSMITTINGNODENUM,  RECIEVINGDATA} state;
 
 void setup()
 {
@@ -42,38 +47,72 @@ void setup()
   rf_driver.init();
   //Setup Reciver LED
   pinMode(LED_RECIEVING, OUTPUT);
+  pinMode(LED_TRANSMITTING, OUTPUT);
   // Setup Serial Monitor
   Serial.begin(9600);
+  //Set State
+  state = TRANSMITTINGNODENUM;
 }
 
 void loop()
 {
-  if(count == 100000){
-    count = 0;
-    Serial.println("Waiting to Recieve Data...");
-    Serial.println("");
-  }
-  count++;
-  // Set buffer to size of expected message
-  // Antenna Test successful,
-  uint8_t buf[9];
-  uint8_t buflen = sizeof(buf);
-  recievedData = (char*)buf;
-  // Check if received packet is correct size
-  if (rf_driver.recv(buf, &buflen))
-  {
-    digitalWrite(LED_RECIEVING, HIGH);
-    Serial.println("Recieveing Data...");
-    processData(recievedData);
+  switch(state){
+     case TRANSMITTINGNODENUM:
+      digitalWrite(LED_TRANSMITTING, HIGH);
+      Serial.println("Transmitting Node Number:");
+      msg = "1";
+      Serial.println(msg);
+      rf_driver.send((uint8_t *)msg, strlen(msg));
+      rf_driver.waitPacketSent();
+      digitalWrite(LED_TRANSMITTING, LOW);
+      delay(1000);
+      if(count == 3){
+        count = 0;
+        state = RECIEVINGDATA;
+      }
+      count ++;
+      break;
 
-  }
-  digitalWrite(LED_RECIEVING,LOW);
+    case RECIEVINGDATA:
+      //Extra code not needed
+      // if(count == 29000){
+      //   count = 0;
+      //   Serial.println("Waiting to Recieve Data...");
+      //   Serial.println("");
+      // }
+      // count++;
+
+      // Set buffer to size of expected message
+      // Antenna Test successful,
+      uint8_t buf[9];
+      uint8_t buflen = sizeof(buf);
+      recievedData = (char*)buf;
+      // Check if received packet is correct size
+      if (rf_driver.recv(buf, &buflen))
+      {
+        digitalWrite(LED_RECIEVING, HIGH);
+        Serial.println("Recieveing Data...");
+        processData(recievedData);
+        digitalWrite(LED_RECIEVING,LOW);
+        Serial.println(count);
+        count = 0;
+        //state = TRANSMITTINGNODENUM;
+      }
+      count ++;
+      digitalWrite(LED_RECIEVING,LOW);
+      //NOTE: create a counter non blocking code to know when to chnage state
+      //Takes arounf 9600-9700 to get a succesfull message  (sometimes 10000 - 20000)
+      break;
+    }
+
 }
 
 //*******************************************************************
 //*******************************************************************
 //*******************************************************************
-//****************************Fucnctions*****************************
+//*****************************Functions*****************************
+
+//Fucntion that processes the data recieved form a single sensor module
 bool processData(String dataR){
   Serial.println("Processing Data Recieved...");
   ind1 = dataR.indexOf(',');                   //finds location of first ,
@@ -102,6 +141,31 @@ bool processData(String dataR){
   Serial.println("");
   Serial.println("");
   return true;
+}
+
+
+//Function just in case something breaks and we need to check if it is hardware
+void testAgreggator(){
+  if(count == 18000){
+    count = 0;
+    Serial.println("Testing connection...");
+    Serial.println("");
+  }
+  count++;
+  // Set buffer to size of expected message
+  // Antenna Test successful,
+  uint8_t buf[9];
+  uint8_t buflen = sizeof(buf);
+  recievedData = (char*)buf;
+  // Check if received packet is correct size
+  if (rf_driver.recv(buf, &buflen))
+  {
+    digitalWrite(LED_RECIEVING, HIGH);
+    Serial.println("Recieveing Data...");
+    processData(recievedData);
+
+  }
+  digitalWrite(LED_RECIEVING,LOW);
 }
 //fucntion ideas-----------
 //Fucntion that takes parmaters of all the nodes and stroes them to read them later
--- Sensor node/src/main.cpp
+++ Sensor node/src/main.cpp
@@ -1,16 +1,12 @@
-#include <Arduino.h>
-//#include <UltraSonicSensor.h>
-//#include <Pins.h>
-//------------------------Trasmitter Code-----------------------
 /*
-  433 MHz RF Module Transmitter Demonstration 1
-  RF-Xmit-Demo-1.ino
-  Demonstrates 433 MHz RF Transmitter Module
-  Use with Receiver Demonstration 1
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
+Sensor Node code for Final Project
+Team 14
+ECE2305
+By Josue Contreras
+Contibutors Fivos, Anastasia, John
 */
 
+#include <Arduino.h>
 // Include RadioHead Amplitude Shift Keying Library
 #include <RH_ASK.h>
 // Include dependant SPI Library
@@ -25,39 +21,93 @@ RH_ASK rf_driver;
 
 //Variables
 String str_out;
+int count;
+const char *msg;
+uint8_t buflen;
+String recievedData;
+enum State {LISTENING,TRANSMITTINGDATA} state;
 
 //Prototypes
 String concatSensorReadings(String nanoName, String oneS, String twoS, String threeS);
+void testSensorNode();
 
 //Object Variables
 UltraSonicSensor sensor1(9,8,30); //MAX: 300 [cm] = 3 [m]
 
 void setup()
 {
-    // Initialize ASK Object
-    rf_driver.init();
-    sensor1.initialize();
-    pinMode(LED_TRANSMITTING, OUTPUT);
-    Serial.begin(9600);
+  // Initialize ASK Object
+  rf_driver.init();
+  sensor1.initialize();
+  pinMode(LED_RECIEVING, OUTPUT);
+  pinMode(LED_TRANSMITTING, OUTPUT);
+  Serial.begin(9600);
+  state = TRANSMITTINGDATA;
 }
 
 void loop()
 {
-    digitalWrite(LED_TRANSMITTING, HIGH);
-    Serial.println("Transmitting...");
-    str_out = concatSensorReadings("N1", "0","0", sensor1.distanceFromPerson());
-    const char *msg = str_out.c_str();
-    //Serial.println(msg);
-    rf_driver.send((uint8_t *)msg, strlen(msg));
-    rf_driver.waitPacketSent();
-    digitalWrite(LED_TRANSMITTING, LOW);
-    delay(1000);
+  switch(state){
+    case LISTENING:
+      if(count == 18000){
+        count = 0;
+        Serial.println("Sensor Node 1 Listening...;)");
+        Serial.println("");
+      }
+      count++;
+      // Set buffer to size of expected message
+      // Antenna Test successful,
+      uint8_t buf[1];
+      buflen = sizeof(buf);
+      recievedData = (char*)buf;
+      // Check if received packet is correct size
+      if (rf_driver.recv(buf, &buflen))
+      {
+        digitalWrite(LED_RECIEVING, HIGH);
+        Serial.println("Authenticating Number...");
+        if(recievedData.equals("1")){
+          state = TRANSMITTINGDATA;
+        }else{
+          Serial.println("Not me...");
+        }
+      }
+      digitalWrite(LED_RECIEVING,LOW);
+      break;
+
+    case TRANSMITTINGDATA:
+      digitalWrite(LED_TRANSMITTING, HIGH);
+      Serial.println("Transmitting...");
+      str_out = concatSensorReadings("N1", "0","0", sensor1.distanceFromPerson());
+      msg = str_out.c_str();
+      //Serial.println(msg);
+      rf_driver.send((uint8_t *)msg, strlen(msg));
+      rf_driver.waitPacketSent();
+      digitalWrite(LED_TRANSMITTING, LOW);
+      delay(1000);
+      break;
+  }
 }
 
 //*******************************************************************
 //*******************************************************************
 //*******************************************************************
-//****************************Fucnctions*****************************
+//*****************************Functions*****************************
+
+//Packages up the data recieved by concatenating the data and adding overhead
 String concatSensorReadings(String nanoName, String oneS, String twoS, String threeS){
   return nanoName + "," + oneS + "," + twoS + "," + threeS + ",";
+}
+
+//Function just in case something breaks and we need to check if it is hardware
+void testSensorNode(){
+
+  digitalWrite(LED_TRANSMITTING, HIGH);
+  Serial.println(" Testing Transmission...");
+  str_out = concatSensorReadings("N1", "0","0", sensor1.distanceFromPerson());
+  msg = str_out.c_str();
+  //Serial.println(msg);
+  rf_driver.send((uint8_t *)msg, strlen(msg));
+  rf_driver.waitPacketSent();
+  digitalWrite(LED_TRANSMITTING, LOW);
+  delay(1000);
 }
